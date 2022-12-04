5d3b6e1c2472685bf41d3b7c43e5d29c477903d0
ThrivingDep <asthaa2024@gmail.com>
2022-12-05 03:06:41 +0530

    basic difference of tagname and classname

--- dom.js
+++ dom.js
@@ -33,14 +33,27 @@
 //titles.textContent = 'ADD ITEM';
 //titles.style.color = 'green';
 
-var items = document.getElementsByClassName('list-group-item');
-console.log('items');
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
+   //var items = document.getElementsByClassName('list-group-item');
+//console.log('items');
+//console.log(items[1]);
+//items[1].textContent = 'Hello 2';
 //items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items[3].style.backgroundColor = 'green';
+//items[1].style.backgroundColor = 'yellow';
+//items[3].style.backgroundColor = 'green';
 
-for(var i =0; i<items.length; i++){
-    items[i].style.fontWeight = 'bold';
+//for(var i =0; i<items.length; i++){
+  //  items[i].style.fontWeight = 'bold';
+//}
+
+//GETELEMENTBYTAGNAME//
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+li[3].style.backgroundColor = 'green';
+
+for(var i =0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
 }
